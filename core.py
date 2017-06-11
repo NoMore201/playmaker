52425c05b3c22a5994ca081ccdb37433f7464d2e
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-06-11 15:51:01 +0200

    Updated search page, added token retrieve
    Signed-off-by: Domenico Iezzi <domenico.iezzi.201@gmail.com>

--- core.py
+++ core.py
@@ -1,18 +1,57 @@
-from ext_libs.googleplay_api.googleplay import GooglePlayAPI
-from ext_libs.googleplay_api.googleplay import config
+from googleplay_api.googleplay import GooglePlayAPI, LoginError
 from flask import Flask, render_template
 import json
+import configparser
+import urllib.request
+from urllib.error import URLError
+
+# parse config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+def get_token():
+    print('Retrieving token from %s' % config['Main']['tokenurl'])
+    r = urllib.request.urlopen(config['Main']['tokenurl'])
+    token = r.read().decode('utf-8')
+    config['Main']['token'] = token
+    with open('config.ini', 'w') as cfgfile:
+        config.write(cfgfile)
+    return token
+
 
 # application setup
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-service = GooglePlayAPI("3d716411bf8bc802", "en_US", True)
-service.login(None, None, config.AUTH_TOKEN)
+service = GooglePlayAPI(config['Main']['id'], "en_US", True)
+try:
+    if config['Main']['token'] == '':
+        try:
+            token = get_token()
+        except URLError:
+            print('Failed to fetch url')
+            print('Try using credentials')
+            quit()
+        service.login(None, None, token)
+    else:
+        service.login(None, None, config['Main']['token'])
+except LoginError:
+    print("Unable to login")
+    quit()
 
 
 @app.route('/')
-def render_page():
+def render_home():
     return render_template('index.html')
+
+
+@app.route('/search')
+def render_search():
+    return render_template('search.html')
+
+
+@app.route('/config')
+def render_config():
+    return render_template('config.html')
 
 
 @app.route('/gplay/search', methods=['POST'])
@@ -28,7 +67,10 @@ def search_app():
             continue
         app = {'title': result.title,
                'developer': result.creator,
-               'version': result.details.appDetails.versionCode}
+               'version': result.details.appDetails.versionCode,
+               'numDownloads': result.details.appDetails.numDownloads,
+               'uploadDate': result.details.appDetails.uploadDate,
+               'stars': '%.2f' % result.aggregateRating.starRating}
         all_apps.append(app)
     return json.dumps(all_apps)
 
--- static/gplay.js
+++ static/gplay.js
@@ -1,28 +1,58 @@
-const logBox = document.getElementById('devlog');
-const tBody = document.getElementById('appsTableBody');
-
-let headers = new Headers();
-headers.append("Content-Type", "text/plain");
-
-fetch('/gplay/search', {
-  method: 'POST',
-  headers: headers
-}).then(response => {
-  if (response.ok) {
-    return response.text();
-  } else {
-    throw Error(response.statusText);
+function fillTable(apps) {
+  let tableBox = document.getElementById('searchTable');
+  let tableHtml = "";
+  tableHtml += '<table class="table" id="appsTable">' +
+    '<thead><tr><th>Name</th><th>Developer</th><th>Version</th>' +
+    '<th>Downloads</th><th>Upload Date</th><th>Stars</th></tr>' +
+    '</thead><tbody id="appsTableBody">';
+  for (var i = 0; i < apps.length; i++) {
+    tableHtml +=
+      '<tr><td>' + apps[i].title + '</td>' +
+      '<td>' + apps[i].developer + '</td>' +
+      '<td>' + apps[i].version + '</td>' +
+      '<td>' + apps[i].numDownloads + '</td>' +
+      '<td>' + apps[i].uploadDate + '</td>' +
+      '<td>' + apps[i].stars + '</td></tr>';
   }
-}).then(text => {
-  console.log(text);
-  let apps = JSON.parse(text);
-  if (apps.length === 0)
-    return;
-  apps.forEach(app => {
-    tBody.innerHTML += '<tr><td>' + app.title + '</td>' +
-                       '<td>' + app.developer + '</td>' +
-                       '<td>' + app.version + '</td></tr>';
+  tableHtml += '</tbody></table>';
+  tableBox.innerHTML = tableHtml;
+}
+
+function search() {
+  let headers = new Headers();
+  headers.append("Content-Type", "text/plain");
+
+  fetch('/gplay/search', {
+    method: 'POST',
+    headers: headers
+  }).then(response => {
+    return response.text();
+  }).then(text => {
+    console.log(text);
+    let apps = JSON.parse(text);
+    if (apps.length === 0)
+      return;
+    fillTable(apps);
+  }).catch(error => {
+    console.log(error);
   });
-}).catch(error => {
-  console.log(error);
-});
+}
+
+function onClickSearch() {
+  let input = document.getElementById('searchInput');
+  console.log(input.value);
+  search();
+}
+
+switch (window.location.pathname) {
+  case '/':
+    console.log('home');
+    break;
+  case '/search':
+    document.getElementById('searchBtn')
+      .onclick = onClickSearch;
+    break;
+  case '/config':
+    console.log('config');
+    break;
+}
