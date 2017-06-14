1e7cc2528d9936f86a445815ca888ffb910b24ff
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-06-14 12:16:42 +0200

    frontend: implemented apk list
    Signed-off-by: Domenico Iezzi <domenico.iezzi.201@gmail.com>

--- static/js/gplay.js
+++ static/js/gplay.js
@@ -1,60 +1,89 @@
-const app = {};
+$(function(){
 
-const appHeaders = new Headers();
-appHeaders.append("Content-Type", "application.json");
+  const app = {};
 
-/*
- * MODELS
- */
+  $.material.init();
 
-app.Apk = Backbone.Model.extend({
-  defaults: {
-    title: '',
-    developer: '',
-    size: '',
-    numDownloads: '',
-    uploadDate: '',
-    docId: '',
-    version: -1,
-    stars: ''
-  }
-});
+  const appHeaders = new Headers();
+  appHeaders.append("Content-Type", "application.json");
 
-/*
- * COLLECTIONS
- */
+  /*
+   * MODELS
+   */
 
-app.ApkView = Backbone.View.extend({
-  template: _.template($('#apk-template').html()),
-  render: function(){
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-});
+  app.Apk = Backbone.Model.extend({
+    defaults: {
+      title: '',
+      developer: '',
+      size: '',
+      numDownloads: '',
+      uploadDate: '',
+      docId: '',
+      version: -1,
+      stars: ''
+    }
+  });
 
-app.AppView = Backbone.View.extend({
-  el: '#container',
-  initialize: function () {
-    this.getLocalApkList();
-  },
-  getLocalApkList: function () {
-    fetch('/gplay/getapks', {
-      method: 'GET',
-      headers: appHeaders
-    }).then(response => {
-      return response.text();
-    }).then(text => {
-      /*
-       * Returned data is an object containing
-       * objects, so we need to iterate through keys
-       */
-      console.log(text);
-      let data = JSON.parse(text);
-      
-    });
-  },
-  events: {
-  }
-});
+  /*
+   * COLLECTIONS
+   */
 
-app.appView = new app.AppView();
+  app.ApkList = Backbone.Collection.extend({
+    model: app.Apk,
+    url: '/gplay/getapps',
+    initialize: function() {}
+  });
+  app.apkList = new app.ApkList();
+
+  app.ApkView = Backbone.View.extend({
+    template: _.template($('#apk-template').html()),
+    render: function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  app.LoadAlertView = Backbone.View.extend({
+    tagName: 'div',
+    id: 'loading-alert',
+    className: 'alert alert-dismissible alert-info',
+    template: '<button type="button" class="close" data-dismiss="alert">×</button>' +
+              '<strong><%- message %></strong>',
+    initialize: function() {
+      this.template = _.template(this.template);
+    },
+    render: function() {
+      let output = this.template({message: 'Loading apks..'});
+      this.$el.html(output);
+      return this;
+    }
+  });
+  app.loadAlertView = new app.LoadAlertView();
+
+  app.AppView = Backbone.View.extend({
+    el: '#container',
+    initialize: function () {
+      $('body').append(app.loadAlertView.render().el);
+      this.getLocalApkList();
+    },
+    getLocalApkList: function () {
+      app.apkList.fetch({
+        success: this.onLoad
+      });
+    },
+    onLoad: function(apks, response) {
+      let load = this.loadAlertView;
+      apks.models.forEach(apk => {
+        let view = new app.ApkView({
+          model: apk
+        });
+        $('#container').append(view.render().el);
+      });
+      app.loadAlertView.remove();
+    },
+    events: {}
+  });
+
+  app.appView = new app.AppView();
+
+}); // end jquery onload function
