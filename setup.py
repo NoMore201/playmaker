8adfff14abaf29488a5351051ee53012a15ead42
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-09-21 12:47:14 +0200

    Updated setup.py + googleplay-api as a dependency
    Signed-off-by: Domenico Iezzi <domenico.iezzi.201@gmail.com>

--- setup.py
+++ setup.py
@@ -1,32 +1,21 @@
 from setuptools import setup
-import os, glob
-
-static = [f for f in glob.glob(os.path.join('templates','*'))]
-templates = [f for f in glob.glob(os.path.join('static', '*'))]
-
-RES_DIR='/usr/share/playmaker'
-
-print(static)
-print(templates)
 
 setup(name='playmaker',
-      version='0.1.0',
-      description='Web wrapper based on gplaycli python3 branch',
+      version='0.3.2',
+      description='Apk manager with web interface based on googleplay-api',
       url='https://github.com/NoMore201/playmaker',
       author='NoMore201',
       author_email='domenico.iezzi.201@gmail.com',
       license='MIT',
-      packages=['googleplay_api'],
-      scripts=['playmaker'],
+      packages=['playmaker'],
+      package_data={
+          'playmaker': ['index.html', 'static/*', 'views/*'],
+      },
       install_requires=[
-            'requests >= 2.0.0',
-            'protobuf',
             'pyaxmlparser',
-            'Flask',
+            'tornado',
+            'googleplay-api',
       ],
-      data_files=[
-          ['/etc', ['playmaker.conf']],
-          [os.path.join(RES_DIR, 'templates'), templates],
-          [os.path.join(RES_DIR, 'static'), static]
-      ]
+      dependency_links=['https://github.com/NoMore201/googleplay-api/tarball/master#egg=googleplay-api'],
+      scripts=['pm-server']
 )
