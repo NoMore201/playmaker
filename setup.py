c6fe46e404d48be707e806a79739385bddd1c172
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-10-05 16:58:29 +0200

    Prepare for v0.4.5
    Signed-off-by: Domenico Iezzi <domenico.iezzi.201@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='playmaker',
-      version='0.4.4',
+      version='0.4.5',
       description='Apk manager with web interface based on googleplay-api',
       url='https://github.com/NoMore201/playmaker',
       author='NoMore201',
