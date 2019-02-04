a97fd9e77ccfabc5f37af1ca93b7b9a3f2048503
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2019-02-04 14:43:09 +0100

    Prepare for v0.6.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='playmaker',
-      version='0.6.3',
+      version='0.6.4',
       description='Apk manager with web interface based on googleplay-api',
       url='https://github.com/NoMore201/playmaker',
       author='NoMore201',
